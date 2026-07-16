cf7102c3dc0eac132c711c3df03a37386172f32f
fuyao <651908384@qq.com>
2026-07-16 19:00:59 +0800

    添加配之文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  // 防止 Vite 清除 Rust 显示的错误
+  clearScreen: false,
+  server: {
+    port: 5173,
+    // Tauri 工作于固定端口，如果端口不可用则报错
+    strictPort: true,
+    // 如果设置了 host，Tauri 则会使用
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 5174,
+        }
+      : undefined,
+    watch: {
+      // 告诉 Vite 忽略监听 `src-tauri` 目录
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+});
